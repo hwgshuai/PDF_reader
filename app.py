79da2cac16ef4b9e096061554cd29faf8ac07a76
zhuruiliang <13173579+python214@user.noreply.gitee.com>
2025-09-10 10:39:45 +0800

    first commit: pdf chatbot with DeepSeek and HuggingFace embeddings

---
+++ app.py
@@ -0,0 +1,97 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_deepseek.chat_models import ChatDeepSeek
+
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+print("DeepSeek API Key:", os.getenv("DEEPSEEK_API_KEY"))
+
+llm = ChatDeepSeek(
+    model="deepseek-chat",
+    temperature=0.3,
+    # other params...
+)
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader  = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000,
+        chunk_overlap=1000,
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceEmbeddings(model_name=r"D:\桌面\AI Projects\chatbot\model")
+    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversation_chain(vector_store):
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = llm
+    prompt = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = HuggingFaceEmbeddings(model_name=r"D:\桌面\AI Projects\chatbot\model")
+    new_db = FAISS.load_local("faiss_index", embeddings = embeddings,allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversation_chain(new_db)
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using DEEPSEEK 💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+if __name__ == "__main__":
+    main()
